08413b077e3d9548c6a1c967088906a772e06183
wpeguero <wpeguerorosario@outlook.com>
2022-10-05 23:56:46 -0400

    CHANGE:Started Skeleton Development of Data
    Finished building the skeleton behind the prediction and loading files
    on the main frame. Moving on to the dashboard frame to plot the data in
    an orderly manner that can be observed.

--- capapp.py
+++ capapp.py
@@ -2,6 +2,10 @@
 import tkinter as tk #Entire module
 from tkinter import ttk #Used for styling the GUI
 from tkinter import filedialog
+from pipeline import obtain_data
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
 
 
 LARGE_FONT = ("Verdana", 12)
@@ -26,9 +30,11 @@ class MyApp(tk.Tk):
 
         # Display the widgets
         self.frames = dict()
-        frame = StartPage(container, self)
-        self.frames[StartPage] = frame
-        frame.grid(row=0, column=0, sticky="nsew")
+        for F in (StartPage, MainDashboard, DetailedDash):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+            
         self.show_frame(StartPage)
     
     def show_frame(self, controller):
@@ -43,10 +49,70 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         """Display the initial widgets."""
         tk.Frame.__init__(self, parent)
-        # Widgets
+        #Labels
         label = tk.Label(self, text="Please load the DICOM files.", font=LARGE_FONT)
         label.grid(row=1, column=5, pady=10, padx=10)
-        cancel_button = tk.Button
+        #Buttons
+        cancel_button = tk.Button(self, text="Cancel", command=lambda:controller.destroy())
+        cancel_button.grid(row=9, column=1, pady=10, padx=10)
+        load_button = tk.Button(self, text="Load Images", command=lambda: self.load_images())
+        load_button.grid(row=9, column=5,pady=10, padx=10)
+        predict_button = tk.Button(self, text="Predict", command=lambda: self.predict(controller))
+        predict_button.grid(row=9, column=9, pady=10, padx=10)
+        self.grid_rowconfigure(5,weight=1)
+        self.grid_columnconfigure(5, weight=1)
+    
+    def load_images(self):
+        """Load Image Names."""
+        self.files_to_review = tk.Listbox(self)
+        self.filename = filedialog.askopenfilenames(initialdir="/", title="Select a File")
+        for name in self.filename:
+            self.files_to_review.insert('end', name)
+        self.files_to_review.grid(row=5, column=5, pady=10, padx=10)
+    
+    def predict(self, controller):
+        """Trigger for the predict action."""
+        data = list(map(obtain_data, self.filename))
+        # Here the predictions will be made
+        predictions = dict()
+        controller.show_frame(MainDashboard)
+        return predictions
+
+class MainDashboard(tk.Frame):
+    """Page used to display the data in a dashboard format."""
+
+    def __init__(self, parent, controller):
+        """Display the initial widgets."""
+        tk.Frame.__init__(self, parent)
+        # Widgets
+        label = tk.Label(self, text="Dashboard", font=LARGE_FONT)
+        label.grid(row=1, column=5, pady=10, padx=10)
+        self.grid_rowconfigure(5,weight=1)
+        self.grid_columnconfigure(5, weight=1)
+        self.plot()
+    
+    def plot(self):
+        """Plots the main graphics of the dashboard."""
+        fig = Figure(figsize=(5,4), dpi=100)
+        ax = fig.add_subplot()
+        #Pseudo data included for example of one graphic
+        label_sex = ["Male", "Female"]
+        sizes = [98, 2]
+        ax.pie(sizes, labels=label_sex)
+        canvas = FigureCanvasTkAgg(fig, master=self)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=2, column=1, pady=10, padx=10)
+
+
+class DetailedDash(tk.Frame):
+    """Page used to zoom in on the image data from the ."""
+
+    def __init__(self, parent, controller):
+        """Display the initial widgets."""
+        tk.Frame.__init__(self, parent)
+        # Widgets
+        label = tk.Label(self, text="Dashboard", font=LARGE_FONT)
+        label.grid(row=1, column=5, pady=10, padx=10)
 
 
 if __name__ == "__main__":
