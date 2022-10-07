c7ceb18893fbe740b66d3c317fbe6d9557507a78
wpeguero <wpeguerorosario@outlook.com>
2022-10-07 19:08:20 -0400

    NEW:Finished the sample models
    Finished the models that will be used to compare and test the
    performance of the original model.

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ def main():
     model.save("tumorclassifier/my_model")
 
 
-class BaseImageClassifier(Model):
+def base_image_classifier(img_height:float, img_width:float):
     """Basic Image Classifier for model comparison improvement.
 
     ...
@@ -29,28 +29,21 @@ class BaseImageClassifier(Model):
     data augmentation is against a model without
     any of these.
     """
+    img_input = Input(shape=(img_height,img_width,3), name="Image Input")
+    cat_input = Input(shape=(5), name="Categorical Input")
+    inputs = [img_input, cat_input]
+    # Set up the images
+    x = Conv2D(16, 3, padding='same', activation='relu')(img_input)
+    x = MaxPooling2D()(x)
+    x = Dense(128, activation='relu')(x)
+    #Set up the categorical data
+    y = Dense(2, activation='relu')(cat_input)
+    # Merge both layers
+    together = concatenate([x,y])
+    output = Dense(2, activation='softmax')(together)
+    return inputs, output
 
-    def __init__(self, img_height:float, img_width:float):
-        """Initialize the layers for the model."""
-        super().__init__(self)
-        self.conv = Conv2D(16, 3, padding='same', activation='relu')
-        self.avgpool = AveragePooling2D()
-        self.flatten = Flatten()
-        self.dropout = Dropout(0.3)
-        self.dense = Dense(128, activation='relu')
-        self.tumor = Dense(2, activation='softmax')
-
-    def call(self, inputs):
-        """Call the layers into action for training."""
-        x = inputs
-        x = self.conv(x)
-        x = self.avgpool(x)
-        x = self.flatten(x)
-        x = self.dropout(x)
-        tumor = self.tumor(x)
-        return tumor
-
-class ImageClassifier(Model):
+def base_image_classifier2(img_height:float, img_width:float):
     """Basic Image Classifier with rescaling and data augmentation.
 
     ...
@@ -60,30 +53,21 @@ class ImageClassifier(Model):
     will include rescaling and data augmentation
     for the sake and purpose of normalizing the data.
     """
-    
-    def __init__(self, img_height:float, img_width):
-        """Initialize the Layers of the model."""
-        super().__init__(self)
-        self.rescale = Rescaling(1./255, input_shape = (img_height, img_width, 3))
-        self.augment = RandomZoom(0.1)
-        self.conv = Conv2D(16, 3, padding='same', activation='relu')
-        self.avgpool = AveragePooling2D()
-        self.flatten = Flatten()
-        self.dropout = Dropout(0.3)
-        self.dense = Dense(128, activation='relu')
-        self.tumor = Dense(2, activation='softmax')
-
-    def call(self, inputs):
-        """Call the layers into action for training."""
-        x = inputs
-        x = self.rescale(x)
-        x = self.augment(x)
-        x = self.conv(x)
-        x = self.avgpool(x)
-        x = self.flatten(x)
-        x = self.dropout(x)
-        tumor = self.tumor(x)
-        return tumor
+    img_input = Input(shape=(img_height,img_width,3), name="Image Input")
+    cat_input = Input(shape=(5), name="Categorical Input")
+    inputs = [img_input, cat_input]
+    # Set up the images
+    x = Rescaling(1./255, input_shape= (img_height, img_width,3))(img_input)
+    x = RandomZoom(0.1)(x)
+    x = Conv2D(16, 3, padding='same', activation='relu')(x)
+    x = MaxPooling2D()(x)
+    x = Dense(128, activation='relu')(x)
+    #Set up the categorical data
+    y = Dense(2, activation='relu')(cat_input)
+    # Merge both layers
+    together = concatenate([x,y])
+    output = Dense(2, activation='softmax')(together)
+    return inputs, output
 
 def tumor_classifier(img_height:float, img_width:float):
     """Complete Tumor Classification Algorithm.
@@ -110,55 +94,11 @@ def tumor_classifier(img_height:float, img_width:float):
     x = Flatten()(x)
     x = Dense(128, activation='relu')(x)
     #Set up the categorical data
-    y = Dense(1)(cat_input)
+    y = Dense(2, activation='relu')(cat_input)
     # Merge both layers
     together = concatenate([x,y])
     output = Dense(2, activation='softmax')(together)
     return inputs, output
-
-
-class TumorClassifier(Model):
-    """Complete Tumor Classification Algorithm.
-
-    ...
-
-    A class containing a simple classifier for any
-    sort of image. The models stemming from this class
-    will include rescaling and data augmentation
-    for the sake and purpose of normalizing the data.
-    """
-    
-    def __init__(self, img_height:float, img_width):
-        """Initialize the layers of the model."""
-        super().__init__(self)
-        self.rescale = Rescaling(1./255, input_shape = (img_height, img_width, 3))
-        self.augment = RandomZoom(0.1)
-        self.conv1 = Conv2D(16, 3, padding='same', activation='relu')
-        self.avgpool1 = AveragePooling2D()
-        self.conv2 = Conv2D(32, 3, padding='same', activation='relu')
-        self.avgpool2 = AveragePooling2D()
-        self.conv3 = Conv2D(64, 3, padding='same', activation='relu')
-        self.avgpool3 = AveragePooling2D()
-        self.flatten = Flatten()
-        self.dropout = Dropout(0.3)
-        self.dense = Dense(128, activation='relu')
-        self.tumor = Dense(2, activation='softmax')
-
-    def call(self, inputs):
-        """Call the layers into action for training."""
-        x = inputs
-        x = self.rescale(x)
-        x = self.augment(x)
-        x = self.conv1(x)
-        x = self.avgpool1(x)
-        x = self.conv2(x)
-        x = self.avgpool2(x)
-        x = self.conv3(x)
-        x = self.avgpool3(x)
-        x = self.flatten(x)
-        x = self.dropout(x)
-        tumor = self.tumor(x)
-        return tumor
 
 
 if __name__ == "__main__":
