d18e8fffc6ab551bf87f99688e32c3266f77edce
wpeguero <wpeguerorosario@outlook.com>
2022-10-15 14:00:32 -0400

    CHANGE: Started Full Training of models
    Specified the batch size of the input data to fix the batch size itself
    while removing dense layers after the Flatten layer. Although the model
    is now fully capable of training itself, it is still not able to save
    itself. Further research is required for saving the machine learning
    model.

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 """This file will contain all of the acutalized models created from the abstract model class(es) made within the base.py file."""
-from keras.layers import Conv2D, Dense, Rescaling, Flatten, MaxPooling2D, Dropout, RandomZoom, AveragePooling2D, Input, concatenate
-from keras.models import Model
-from keras.utils import plot_model
-from keras.losses import SparseCategoricalCrossentropy
-from keras.metrics import SparseCategoricalAccuracy
+from tensorflow.keras.layers import Conv2D, Dense, Rescaling, Flatten, MaxPooling2D, Dropout, RandomZoom, Permute, Reshape, Input, Concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.metrics import SparseCategoricalAccuracy
 import pipeline as pl
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -11,16 +11,20 @@ import tensorflow as tf
 import numpy as np
 from pipeline import load_data
 
+BATCH_SIZE = 6
+
 def main():
-    inputs, output = tumor_classifier(256,256)
+    inputs, output = tumor_classifier(256, 256, BATCH_SIZE)
     model = Model(inputs=inputs, outputs=output)
     #model.build(input_shape=(800,800))
-    #plot_model(model, show_shapes=True)
+    plot_model(model, show_shapes=True)
+    
     model.compile(optimizer='adam', loss=SparseCategoricalCrossentropy(from_logits=True), metrics=[SparseCategoricalAccuracy()])
     filename = "data/CMMD-set/medical_data_with_image_paths.csv"
-    X, y = load_data(filename)
+    X, y = load_data(filename, batch_size=BATCH_SIZE)
     Xy = tf.data.Dataset.zip((X,y))
-    model.fit(Xy, epochs=10, batch_size=32)
+    model.fit(Xy, epochs=10)
+    model.save('models/tclass_V1')
 
 
 def base_image_classifier(img_height:float, img_width:float):
@@ -79,7 +83,7 @@ def base_image_classifier2(img_height:float, img_width:float):
     output = Dense(2, activation='softmax')(together)
     return inputs, output
 
-def tumor_classifier(img_height:float, img_width:float):
+def tumor_classifier(img_height:float, img_width:float, batch_size:int):
     """Complete Tumor Classification Algorithm.
 
     ...
@@ -89,26 +93,26 @@ def tumor_classifier(img_height:float, img_width:float):
     will include rescaling and data augmentation
     for the sake and purpose of normalizing the data.
     """
-    img_input = Input(shape=(img_height,img_width,3), name="Image Input")
-    cat_input = Input(shape=(2), name="Categorical Input")
+    img_input = Input(shape=(img_height, img_width, 3), batch_size=batch_size, name="Image Input")
+    cat_input = Input(shape=(2), batch_size=batch_size, name="Categorical Input")
     inputs = [img_input, cat_input]
     # Set up the images
-    x = Rescaling(1./255, input_shape= (img_height, img_width,3))(img_input)
-    x = RandomZoom(0.1)(x)
+    x = Rescaling(1./255, input_shape=(img_height, img_width,3))(img_input)
+    #x = RandomZoom(0.1)(x)
+    x = Conv2D(16, 3, padding='same', activation='relu')(x)
+    x = MaxPooling2D()(x)
     x = Conv2D(32, 3, padding='same', activation='relu')(x)
     x = MaxPooling2D()(x)
     x = Conv2D(64, 3, padding='same', activation='relu')(x)
     x = MaxPooling2D()(x)
-    x = Conv2D(64, 3, padding='same', activation='relu')(x)
-    x = MaxPooling2D()(x)
     x = Flatten()(x)
-    print(x)
-    x = Dense(64, activation='relu')(x)
     #Set up the categorical data
     y = Dense(2, activation='relu')(cat_input)
+    y = Dense(1, activation='relu')(y)
     # Merge both layers
-    together = concatenate([x,y])
-    output = Dense(2, activation='softmax', name="classification")(together)
+
+    together = Concatenate(axis=1)([x,y])
+    output = Dense(2, activation='sigmoid', name="classification")(together)
     return inputs, output
 
 
