962cc5eaba5aac5a5ff39089df62772bdf7d50a7
wpeguero <wpeguerorosario@outlook.com>
2022-10-20 18:06:10 -0400

    NEW:Dash iteration of the application.
    Started creating an online version of the app that will extract the
    information from uploaded DICOM files and make predictions based on the
    data extracted.

---
+++ capapp3.py
@@ -0,0 +1,40 @@
+"""Create a Dash Application.
+
+Create an application where the user is able to upload
+DICOM data, where a machine learning model will then
+make predictions. Once the model finishes its predictions,
+a dashboard page will load to show the resulting data.
+There will be six total charts (three pie charts and
+three bar graphs).
+"""
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
