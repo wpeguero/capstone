8cd109c7eba522b1ae71c5a81b24b7d1fe940a6e
wpeguero <wpeguerorosario@outlook.com>
2022-10-23 22:03:33 -0400

    NEW: Finished inclusion of data table.
    Finally able to properly upload and display the data into a table. Now
    that the table is being displayed on the screen, the algorithm should be
    able to plot the six figures from the dashboard on a side by side.

--- capapp3.py
+++ capapp3.py
@@ -12,8 +12,32 @@ import plotly.express as px
 import pydicom as pdc
 from pandas import DataFrame
 from PIL import Image
+from numpy import asarray, argmax
 
 from pipeline import extract_data, transform_data
+
+#Base data
+modalities = {
+    0: 'MR',
+    1: 'CT',
+    2: 'PT',
+    3: 'MG'
+}
+
+sides = {
+    0: 'L',
+    1: 'R'
+}
+
+sex = {
+    0: 'F',
+    1: 'M'
+}
+
+class_names = {
+    0: 'Benign',
+    1: 'Malignant'
+}
 
 app = Dash(__name__)
 
@@ -26,7 +50,7 @@ app.layout = html.Div([
     dcc.Upload(
         id='upload-data',
         children=html.Div([
-            'Drag and Drop or',
+            'Drag and Drop or ',
             html.A('Select Files')
         ]),
     style={
@@ -76,26 +100,12 @@ def parse_contents(contents, filename, date):
         ds = pdc.dcmread(io.BytesIO(decoded))
         datapoint = extract_data(ds)
         datapoint = transform_data(datapoint)
-        img_arr = ds.pixel_array
-        img = Image.fromarray(img_arr)
     except Exception as e:
         print(e)
         return html.Div([
             'There was an error processing this file.'
         ])
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
-        html.Img(src=img),
-        html.Hr(), #Horizontal Line
-
-        # For debugging
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
+    return datapoint
 
 @app.callback(
     Output('output-data-upload', 'children'),
@@ -104,10 +114,40 @@ def parse_contents(contents, filename, date):
     State('upload-data', 'last_modified'))
 def update_output(list_of_contents, list_of_names, list_of_dates): #Need to change this to collect all data.
     if list_of_contents is not None:
-        children = [
+        data = DataFrame([
             parse_contents(c, n, d) for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
-        ]
-        return children
+        ])
+        model = load_model('./tclass_V1')
+        predictions = model.predict({'image': asarray(data['Image'].to_list()), 'cat':asarray(data[['age', 'side']])})
+        if len(predictions) < 2 and len(predictions) > 0:
+            predictions = predictions[0]
+            data['score'] = [softmax(predictions).numpy()]
+            data['pred_class'] = class_names[argmax(data['score'])]
+        elif len(predictions) >= 2:
+            predictions = predictions
+            pred_data = list()
+            for pred in predictions:
+                score = softmax(pred)
+                pclass = class_names[argmax(score)]
+                pred_data.append({'score':score, 'pred_class':pclass})
+            _df = DataFrame(pred_data)
+            data = data.join(_df)
+        data = data.drop(columns='Image')
+        first_column = data.pop('Subject ID')
+        data.insert(0, 'Suject ID', first_column)
+        data['score'] = data['score'].astype(str)
+        return html.Div([
+            html.H1(children='Main Dashboard', style={'textAlign': 'center'}),
+            html.H5('Data Table'),
+            html.H6(datetime.datetime.now()),
+
+            dash_table.DataTable(
+                data.to_dict('records'),
+                [{'name':i, 'id':i} for i in data.columns]
+            ),
+            html.Hr(),
+            html.Div('Raw Content'),
+        ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
