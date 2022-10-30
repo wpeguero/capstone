4e89b3029ea3222ed635f12917b4be82d6537aa5
wpeguero <wpeguerorosario@outlook.com>
2022-10-30 13:11:07 -0400

    CHANGE:Developed new model
    Made corrections to the data which allowed me to observe the increase in
    training. Currently encountering an issue where the data does not allow
    for better accuracy between models. For this reason the RMSprop
    optimizer is being used at the moment. The next possible changes in
    design may be attributed to the model architecture now that I have
    discovered how to prevent the padding from occuring between
    convolutions.

--- models.py
+++ models.py
@@ -1,41 +1,51 @@
-"""This file will contain all of the actualized models created from the abstract model class(es) made within the base.py file."""
+"""Models Module
+-----------------
+
+This file will contain all of the actualized models
+created from the abstract model class(es) made within
+the base.py file."""
 from tensorflow.keras.layers import Conv2D, Dense, Rescaling, Flatten, MaxPooling2D, Dropout, RandomZoom, Permute, Reshape, Input, Concatenate
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
-from tensorflow.keras.metrics import SparseCategoricalAccuracy
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.metrics import CategoricalAccuracy
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import plot_model, split_dataset
-from tensorflow.saved_model import save
+from tensorflow.keras.models import save_model
 import tensorflow as tf
 from pipeline import load_data, load_data2
+import datetime
 
-BATCH_SIZE = 7
+BATCH_SIZE = 8
 
-def main():
-    inputs, output = tumor_classifier(1147, 957, BATCH_SIZE)
+def _main():
+    inputs, output = tumor_classifier(1147, 957)
     model = Model(inputs=inputs, outputs=output)
     #model.build(input_shape=(800,800))
-    plot_model(model, show_shapes=True)
+    plot_model(model, show_shapes=True, to_file='model6.png')
     
-    model.compile(optimizer='adam', loss=SparseCategoricalCrossentropy(), metrics=[SparseCategoricalAccuracy()])
+    model.compile(optimizer='RMSprop', loss=CategoricalCrossentropy(), metrics=[CategoricalAccuracy()])
     filename = "data/CMMD-set/clinical_data_with_unique_paths.csv"
-    data = load_data2(filename)
-    y = data['class']
-    data.pop('class')
-    #print("This is the number of samples within the dataset {}.".format(len(y)))
-    #X, y = load_data(filename, batch_size=BATCH_SIZE)
-    #Xy = tf.data.Dataset.zip((X,y)).cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
-    #Xylength = tf.data.experimental.cardinality(Xy)
-    #Xlength = tf.data.experimental.cardinality(X)
-    #ylength = tf.data.experimental.cardinality(y)
-    #print(Xylength.numpy())
-    #print(Xlength.numpy())
-    #print(ylength.numpy())
-    dataset = tf.data.Dataset.from_tensor_slices((data, y))
-    tf.data.Dataset.save(dataset, 'data/CMMD-set/saved_data2')
-    exit()
-    dataset = tf.data.Dataset.load('data/CMMD-set/saved_data')
-    model.fit(dataset, epochs=10, batch_size=BATCH_SIZE)
-    save(model,'tclass_V1')
+    tfrecordname = 'data/CMMD-set/saved_data3'
+    tfrecordname = None
+    if tfrecordname is None:
+        print("\nLoading data for training...\n")
+        data = load_data2(filename)
+        y = data['class']
+        data.pop('class')
+        dataset = tf.data.Dataset.from_tensor_slices((data, y)).batch(BATCH_SIZE)
+    else:
+        print("\nLoading TFRecord...\n")
+        dataset = tf.data.Dataset.load('data/CMMD-set/saved_data3')
+    dataset = dataset.shuffle(buffer_size=1_000).prefetch(tf.data.AUTOTUNE)
+    tf.data.Dataset.save(dataset, 'data/CMMD-set/saved_data3')
+    #trainds = dataset.random(seed=4).take(2808)
+    #testds = dataset.random(seed=4).skip(2808)
+    #trainds = trainds.shuffle(buffer_size=1_000).prefetch(tf.data.AUTOTUNE)
+    #exit()
+    #logs = './data/trainlogs/' + datetime.datetime.now().strftime("%Y%m%d - %H%M%S")
+    #tb_callback1 = tf.keras.callbacks.TensorBoard(log_dir=logs, histogram_freq=1, profile_batch=20)
+    #tb_callback2 = tf.keras.callbacks.TensorBoard(log_dir=logs, histogram_freq=1, profile_batch=40)
+    model.fit(dataset, epochs=100)
+    save_model(model,'./models/tclass_V10')
 
 
 def base_image_classifier(img_height:float, img_width:float):
@@ -90,8 +100,8 @@ def base_image_classifier(img_height:float, img_width:float):
         to create the model using TensorFlow's Model
         class.
     """
-    img_input = Input(shape=(img_height,img_width,3), name="Image Input")
-    cat_input = Input(shape=None, name="Categorical Input")
+    img_input = Input(shape=(img_height,img_width,1), name="image")
+    cat_input = Input(shape=(2), name="cat")
     inputs = [img_input, cat_input]
     # Set up the images
     x = Conv2D(16, 3, padding='same', activation='relu')(img_input)
@@ -170,7 +180,56 @@ def base_image_classifier2(img_height:float, img_width:float):
     output = Dense(2, activation='softmax')(together)
     return inputs, output
 
-def tumor_classifier(img_height:float, img_width:float, batch_size:int):
+def base_tumor_classifier(img_height:float, img_width:float):
+    """Base Tumor Classification Algorithm.
+
+    ...
+
+    A class containing a simple classifier for side-view
+    image. The models stemming from this class
+    will include rescaling and data augmentation
+    for the sake and purpose of normalizing the data.
+
+    Parameters
+    -----------
+    img_height : float
+        The height, in pixels, of the input images.
+        This can be the maximum height of all images
+        within the dataset to fit a varied amount
+        that is equal or less than the declared height.
+    
+    img_width : float
+        The width, in pixels, of the input images.
+        This can also be the maximum width of all
+        images within the dataset to fit a varied
+        amount that is equal or smaller in width
+        to the declared dimension.
+    
+    Returns
+    -------
+    inputs : {img_input, cat_input}
+        Input layers set to receive both image and
+        categorical data. The image input contains
+        images in the form of a 2D numpy array. The
+        categorical input is a 1D array containing
+        patient information. This is mainly comprised
+        of categorical data, but some nominal data.
+    
+    output : Dense Layer
+        The last layer of the model developed. As
+        the model is fed through as the input of
+        the next layer, the last layer is required
+        to create the model using TensorFlow's Model
+        class.
+    """
+    img_input = Input(shape=(img_height, img_width, 1), name='image')
+    cat_input = Input(shape=(2), name='cat')
+    inputs = [img_input, cat_input]
+    # Set up the images
+    x = Rescaling(1./255, input_shape=(img_height, img_width,1))(img_input)
+    # Finish building the model
+
+def tumor_classifier(img_height:float, img_width:float):
     """Complete Tumor Classification Algorithm.
 
     ...
@@ -195,7 +254,7 @@ def tumor_classifier(img_height:float, img_width:float, batch_size:int):
         amount that is equal or smaller in width
         to the declared dimension.
     
-    batch_size : int
+    batch_size : int *
         One of the factors of the total sample size.
         This is done to better train the model without
         allowing the model to memorize the data.
@@ -216,27 +275,31 @@ def tumor_classifier(img_height:float, img_width:float, batch_size:int):
         the next layer, the last layer is required
         to create the model using TensorFlow's Model
         class.
+    
+    ---
+    
+    *Deprecated
     """
-    img_input = Input(shape=(img_height, img_width, 1), batch_size=batch_size, name='image')
-    cat_input = Input(shape=(2), batch_size=batch_size, name='cat')
+    img_input = Input(shape=(img_height, img_width, 1), name='image')
+    cat_input = Input(shape=(2), name='cat')
     inputs = [img_input, cat_input]
     # Set up the images
     x = Rescaling(1./255, input_shape=(img_height, img_width,1))(img_input)
-    #x = RandomZoom(0.1)(x)
-    x = Conv2D(16, 3, padding='same', activation='relu')(x)
+    x = Conv2D(128, 3, padding='valid', activation='sigmoid')(x)
     x = MaxPooling2D()(x)
-    x = Conv2D(32, 3, padding='same', activation='relu')(x)
+    x = Conv2D(128, 3, padding='valid', activation='sigmoid')(x)
     x = MaxPooling2D()(x)
-    x = Conv2D(64, 3, padding='same', activation='relu')(x)
+    x = Conv2D(128, 3, padding='valid', activation='sigmoid')(x)
+    x = MaxPooling2D()(x)
+    x = Conv2D(64, 3, padding='valid', activation='sigmoid')(x)
     x = MaxPooling2D()(x)
     x = Flatten()(x)
-    #x = Dense(512, activation='relu')(x)
-    #x = Dense(256, activation='relu')(x)
-    #x = Dense(128, activation='relu')(x)
-    #x = Dense(64, activation='relu')(x)
-    #x = Dense(32, activation='relu')(x)
-    #x = Dense(16, activation='relu')(x)
-    #x = Dense(8, activation='relu')(x)
+    x = Dense(1023, activation='relu')(x)
+    #x = Dropout(0.3)(x)
+    x = Dense(341, activation='relu')(x)
+    x = Dense(33, activation='relu')(x)
+    x = Dense(11, activation='relu')(x)
+    x = Dense(2, activation='relu')(x)
     #Set up the categorical data
     y = Dense(2, activation='relu')(cat_input)
     y = Dense(1, activation='relu')(y)
@@ -248,4 +311,4 @@ def tumor_classifier(img_height:float, img_width:float, batch_size:int):
 
 
 if __name__ == "__main__":
-    main()
+    _main()
