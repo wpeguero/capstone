bc287199e9b67e21971f516b5dbbd3954ae51b39
wpeguero <wpeguerorosario@outlook.com>
2022-11-04 10:16:24 -0400

    CHANGE:Created the base models for comparison
    Created a set of base models meant to be used for comparing performance
    between the base models and the finalized models. The base models so far
    are two, one model that does not include the factor of age into the
    calculation and another that does include age into the factors, but does
    not use CNN architecture.

--- models.py
+++ models.py
@@ -17,10 +17,10 @@ import datetime
 BATCH_SIZE = 5
 
 def _main():
-    inputs, output = tumor_classifier(1147, 957)
+    inputs, output = base_image_classifier(1147, 957)
     model = Model(inputs=inputs, outputs=output)
     #model.build(input_shape=(800,800))
-    plot_model(model, show_shapes=True, to_file='./models/model_architechtures/model_AlexNet_Final.png')
+    plot_model(model, show_shapes=True, to_file='./models/model_architechtures/model_AlexNet_Bimage.png')
     
     model.compile(optimizer='SGD', loss=CategoricalCrossentropy(), metrics=[CategoricalAccuracy()])
     filename = "data/CMMD-set/clinical_data_with_unique_paths.csv"
@@ -45,7 +45,7 @@ def _main():
     #tb_callback1 = tf.keras.callbacks.TensorBoard(log_dir=logs, histogram_freq=1, profile_batch=20)
     #tb_callback2 = tf.keras.callbacks.TensorBoard(log_dir=logs, histogram_freq=1, profile_batch=40)
     model.fit(dataset, epochs=100)
-    save_model(model,'./models/tclass_AlexNet_Final')
+    save_model(model,'./models/tclass_AlexNet_Bimage')
 
 
 def base_image_classifier(img_height:float, img_width:float):
@@ -93,7 +93,7 @@ def base_image_classifier(img_height:float, img_width:float):
         patient information. This is mainly comprised
         of categorical data, but some nominal data.
     
-    output : Dense Layer
+    x : Dense Layer
         The last layer of the model developed. As
         the model is fed through as the input of
         the next layer, the last layer is required
@@ -101,84 +101,31 @@ def base_image_classifier(img_height:float, img_width:float):
         class.
     """
     img_input = Input(shape=(img_height,img_width,1), name="image")
-    cat_input = Input(shape=(2), name="cat")
-    inputs = [img_input, cat_input]
     # Set up the images
-    x = Conv2D(16, 3, padding='same', activation='relu')(img_input)
-    x = MaxPooling2D()(x)
+    x = Conv2D(96, 7, strides=(2,2), activation='relu')(img_input)
+    x = MaxPooling2D(pool_size=(3,3), strides=2)(x)
+    x = BatchNormalization()(x)
+    x = Conv2D(256, 5, strides=(2,2), activation='relu')(x)
+    x = MaxPooling2D(pool_size=(3,3), strides=2)(x)
+    x = BatchNormalization()(x)
+    x = Conv2D(384, 3, padding='same', activation='relu')(x)
+    #x = Dropout(0.3)(x)
+    x = Conv2D(384, 3, padding='same', activation='relu')(x)
+    x = Conv2D(256, 3, padding='same', activation='relu')(x)
+    x = MaxPooling2D(pool_size=(3,3), strides=2)(x)
     x = Flatten()(x)
-    #x = Dense(128, activation='relu')(x)
-    #Set up the categorical data
-    y = Dense(2, activation='relu')(cat_input)
-    y = Dense(1, activation='relu')(y)
-    # Merge both layers
-    together = Concatenate(axis=1)([x,y])
-    output = Dense(2, activation='softmax', name="output")(together)
-    return inputs, output
-
-def base_image_classifier2(img_height:float, img_width:float):
-    """Basic Image Classifier with rescaling and data augmentation.
-
-    ...
-
-    A class containing a simple classifier for any
-    sort of image. The models stemming from this class
-    will include rescaling and data augmentation
-    for the sake and purpose of normalizing the data.
-    
-    Parameters
-    -----------
-    img_height : float
-        The height, in pixels, of the input images.
-        This can be the maximum height of all images
-        within the dataset to fit a varied amount
-        that is equal or less than the declared height.
-    
-    img_width : float
-        The width, in pixels, of the input images.
-        This can also be the maximum width of all
-        images within the dataset to fit a varied
-        amount that is equal or smaller in width
-        to the declared dimension.
-    
-    batch_size : int
-        One of the factors of the total sample size.
-        This is done to better train the model without
-        allowing the model to memorize the data.
-    
-    Returns
-    -------
-    inputs : {img_input, cat_input}
-        Input layers set to receive both image and
-        categorical data. The image input contains
-        images in the form of a 2D numpy array. The
-        categorical input is a 1D array containing
-        patient information. This is mainly comprised
-        of categorical data, but some nominal data.
-    
-    output : Dense Layer
-        The last layer of the model developed. As
-        the model is fed through as the input of
-        the next layer, the last layer is required
-        to create the model using TensorFlow's Model
-        class.
-    """
-    img_input = Input(shape=(img_height,img_width,3), name="Image Input")
-    cat_input = Input(shape=(5), name="Categorical Input")
-    inputs = [img_input, cat_input]
-    # Set up the images
-    x = Rescaling(1./255, input_shape= (img_height, img_width,3))(img_input)
-    x = RandomZoom(0.1)(x)
-    x = Conv2D(16, 3, padding='same', activation='relu')(x)
-    x = MaxPooling2D()(x)
-    #x = Dense(128, activation='relu')(x)
-    #Set up the categorical data
-    y = Dense(2, activation='relu')(cat_input)
-    y = Dense(1, activation='relu')(y)
-    # Merge both layers
-    together = Concatenate(axis=1)([x,y])
-    output = Dense(2, activation='softmax')(together)
-    return inputs, output
+    x = Dense(4096, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(4096, activation='relu')(x)
+    x = Dense(1000, activation='relu')(x)
+    x = Dense(500, activation='relu')(x)
+    x = Dense(250, activation='relu')(x)
+    x = Dense(100, activation='relu')(x)
+    x = Dense(50, activation='relu')(x)
+    x = Dense(25, activation='relu')(x)
+    x = Dense(12, activation='relu')(x)
+    x = Dense(2, activation='sigmoid')(x)
+    return img_input, x
 
 def base_tumor_classifier(img_height:float, img_width:float):
     """Base Tumor Classification Algorithm.
@@ -187,8 +134,8 @@ def base_tumor_classifier(img_height:float, img_width:float):
 
     A class containing a simple classifier for side-view
     image. The models stemming from this class
-    will include rescaling and data augmentation
-    for the sake and purpose of normalizing the data.
+    will include rescaling for the sake and purpose
+    of normalizing the data.
 
     Parameters
     -----------
@@ -227,7 +174,24 @@ def base_tumor_classifier(img_height:float, img_width:float):
     inputs = [img_input, cat_input]
     # Set up the images
     x = Rescaling(1./255, input_shape=(img_height, img_width,1))(img_input)
-    # Finish building the model
+    x = Conv2D(96, 3, strides=(2,2), activation='relu')(x)
+    x = MaxPooling2D(pool_size=(3,3), strides=2)(x)
+    x = BatchNormalization()(x)
+    x = Flatten()(x)
+    x = Dropout(0.3)(x)
+    x = Dense(250, activation='relu')(x)
+    x = Dense(100, activation='relu')(x)
+    x = Dense(50, activation='relu')(x)
+    x = Dense(25, activation='relu')(x)
+    #Set up the categorical data
+    y = Dense(2, activation='relu')(cat_input)
+    y = Dense(1, activation='relu')(y)
+    # Merge both layers
+
+    together = Concatenate(axis=1)([x,y])
+    together = Dense(13, activation='relu')(together)
+    output = Dense(2, activation='sigmoid', name='class')(together)
+    return inputs, output
 
 def tumor_classifier(img_height:float, img_width:float):
     """Complete Tumor Classification Algorithm.
