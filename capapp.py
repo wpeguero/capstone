8e133d78293704cc2be5723a757f61acf6de47b8
wpeguero <wpeguerorosario@outlook.com>
2022-11-07 22:04:09 -0500

    CHANGE:Renamed capapp3 to capapp.
    Decided to remove the two previous iterations of the capapp and begin
    once again with the use of the original name (capapp). This was to spare
    any confusion.

--- capapp.py
+++ capapp.py
@@ -1,143 +1,149 @@
-"""Software behind the Capstone Project GUI."""
-import tkinter as tk #Entire module
-from tkinter import ttk #Used for styling the GUI
-from tkinter import filedialog
-from pipeline import extract_data
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
-from matplotlib.backend_bases import key_press_handler
-from matplotlib.figure import Figure
-import os
+"""The third Iteration of the Capstone Project Application.
 
-LARGE_FONT = ("Verdana", 12)
+After failing to create the offline GUI through the use of
+Tkinter library and matplotlib, an online version of the
+same GUI has been replicated using the Dash library. This
+library uses the plotly library on the backend to develop
+plots and display them within html that the Dash library
+develops.
+"""
+import base64
+import datetime
+import io
 
-def main():
-    """Quick Testing Phase."""
-    testObj = MyApp()
-    testObj.mainloop()
+from dash import Dash, dcc, html, dash_table
+from dash.dependencies import Input, Output, State
 
-
-class MyApp(tk.Tk):
-    """Cancer Classification Data Product."""
-
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        #initial data
-        self.data = list()
-        self.wm_title("The Capstone Project")
-        self.minsize(900,600)
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        # Display the widgets
-        self.frames = dict()
-        for F in (StartPage, MainDashboard, DetailedDash):
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-            
-        self.show_frame(StartPage)
-    
-    def show_frame(self, controller):
-        """Display the page based on selection."""
-        frame = self.frames[controller]
-        frame.tkraise()
+import plotly.express as px
+from plotly.subplots import make_subplots
+import pydicom as pdc
+from pandas import DataFrame
 
 
-class StartPage(tk.Frame):
-    """Page used to load the DICOM files."""
-
-    def __init__(self, parent, controller):
-        """Display the initial widgets."""
-        tk.Frame.__init__(self, parent)
-        self.data = list()
-        self.filename = list()
-        #Labels
-        label = tk.Label(self, text="Please load the DICOM files.", font=LARGE_FONT)
-        label.grid(row=1, column=5, pady=10, padx=10)
-        #Buttons
-        cancel_button = tk.Button(self, text="Cancel", command=lambda:controller.destroy())
-        cancel_button.grid(row=9, column=1, pady=10, padx=10)
-        load_button = tk.Button(self, text="Load Images", command=lambda: self.load_images())
-        load_button.grid(row=9, column=5,pady=10, padx=10)
-        predict_button = tk.Button(self, text="Predict", command=lambda: self.predict(controller))
-        predict_button.grid(row=9, column=9, pady=10, padx=10)
-        self.grid_rowconfigure(5,weight=1)
-        self.grid_columnconfigure(5, weight=1)
-    
-    def load_images(self):
-        """Load Image Names."""
-        self.files_to_review = tk.Listbox(self)
-        self.filename = filedialog.askopenfilenames(initialdir="/", title="Select a File")
-        for name in self.filename:
-            self.files_to_review.insert('end', os.path.basename(name))
-        self.files_to_review.grid(row=5, column=5, pady=10, padx=10)
-        return self
-    
-    def predict(self, controller):
-        """Trigger for the predict action."""
-        self.data = list(map(extract_data, self.filename))
-        # Here the predictions will be made
-        controller.predictions = dict()
-        MainDashboard.plot(controller)
-        controller.show_frame(MainDashboard)
-        MainDashboard.plot(controller)
-
-class MainDashboard(tk.Frame):
-    """Page used to display the data in a dashboard format."""
-
-    def __init__(self, parent, controller):
-        """Display the initial widgets."""
-        tk.Frame.__init__(self, parent)
-        # Widgets
-        label = tk.Label(self, text="Dashboard", font=LARGE_FONT)
-        label.grid(row=1, column=5, pady=10, padx=10)
-        self.grid_rowconfigure(5,weight=1)
-        self.grid_columnconfigure(5, weight=1)
-    
-    @staticmethod
-    def plot(self):
-        """Plots the main graphics of the dashboard."""
-        fig = Figure(figsize=(9,9), dpi=100)
-        #First Pie Chart
-        ax1 = fig.add_subplot(231)
-        label_sex = ["Male", "Female"]
-        print(self.data)
-        sizes = self.data['sex']
-        ax1.pie(sizes, labels=label_sex)
-        #Second Pie Chart
-        ax2 = fig.add_subplot(232)
-        pred_labels = ["Yes", "No"] #Pseudo Data
-        pred_count = [65, 35]
-        ax2.pie(pred_count, labels=pred_labels)
-        #Third Pie Chart
-        ax3 = fig.add_subplot(233)
-        label_modality = ['MRI', 'CT', 'PT', 'Other']
-        modality_count = [30, 40, 20, 10]
-        ax3.pie(modality_count, labels=label_modality)
-        #First Bar Graph
-        ax4 = fig.add_subplot(234)
-        label_age = ['20-29', '30-39', '40-49', '50-59', '60-69']
-        age_count = [3, 5, 8, 10, 6]
-        ax4.bar(label_age, age_count)
-        #Load the Chart on the GUI
-        canvas = FigureCanvasTkAgg(fig, master=self)
-        canvas.draw()
-        canvas.get_tk_widget().grid(row=2, column=5, pady=10, padx=10)
+from pipeline import extract_data, transform_data, predict
 
 
-class DetailedDash(tk.Frame):
-    """Page used to zoom in on the image data from the ."""
 
-    def __init__(self, parent, controller):
-        """Display the initial widgets."""
-        tk.Frame.__init__(self, parent)
-        # Widgets
-        label = tk.Label(self, text="Dashboard", font=LARGE_FONT)
-        label.grid(row=1, column=5, pady=10, padx=10)
+app = Dash(__name__)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+    style={
+        'width': '100%',
+        'height': '60px',
+        'lineHeight': '60px',
+        'borderWidth': '1px',
+        'borderStyle': 'dashed',
+        'borderRadius': '5px',
+        'textAlign': 'center',
+        'margin': '10px'
+    },
+    multiple=True
+    ),
+    html.Div(id='output-data-upload')
+])
+
+def parse_contents(contents, filename, date):
+    """Load the content.
+
+    Function set to load the uploaded content and
+    transform the data into a readable format for
+    the machine learning model to make predictions.
+
+    Parameters
+    ----------
+    contents : Unkown
+        Contains the uploaded file encoded into its
+        64-bit version. This is then decoded and read
+        using the pydicom library for data processing.
+
+    filename : string
+        The name of the uploaded file. this does
+        not impact the data processing aspect or
+        the predictions, but it is necessary to
+        point out which file the user is looking
+        at.
+
+    date : datetime
+        Used to let the user know the date in which
+        the file was uploaded. Will be used within
+        report to download.
+    """
+    content_type, content = contents.split(',')
+    decoded = base64.b64decode(content)
+    try:
+        ds = pdc.dcmread(io.BytesIO(decoded))
+        datapoint = extract_data(ds)
+        datapoint = transform_data(datapoint)
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+    return datapoint
 
 
-if __name__ == "__main__":
-    main()
-#https://www.digitalocean.com/community/tutorials/tkinter-working-with-classes
+@app.callback(
+    Output('output-data-upload', 'children'),
+    Input('upload-data', 'contents'),
+    State('upload-data', 'filename'),
+    State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates): #Need to change this to collect all data.
+    """Load the main dashboard."""
+    if list_of_contents is not None:
+        data = DataFrame([
+            parse_contents(c, n, d) for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
+        ])
+        data = predict(data, "models/tclass_AlexNet_Final")
+        data = data.drop(columns='Image')
+        first_column = data.pop('Subject ID')
+        data.insert(0, 'Suject ID', first_column)
+        data['score'] = data['score'].astype(str)
+        #Plotting the data.
+        fig = make_subplots(
+            rows=2, cols=2
+        )
+        fig1 = px.pie(data, names='pred_class')
+        fig2 = px.pie(data, names='sex')
+        fig3 = px.pie(data, names='side')
+        fig4 = px.bar(data, x='age')
+        fig5 = px.bar(data, x='sex', y='pred_class')
+        return html.Div([
+            html.H1(children='Main Dashboard', style={'textAlign': 'center'}),
+            html.H5('Data Table'),
+            html.H6(datetime.datetime.now()),
+            html.Div([
+                html.Div([
+                    dcc.Graph(id='g1', figure=fig1, style={'display': 'inline-block', 'width':'33vw'}),
+                    dcc.Graph(id='g2', figure=fig2, style={'display': 'inline-block', 'width':'33vw'}),
+                    dcc.Graph(id='g3', figure=fig3, style={'display': 'inline-block', 'width':'33vw'}),
+                ]),
+                html.Div(children=[
+                    dcc.Graph(id='g4', figure=fig4, style={'display': 'inline-block'}),
+                    dcc.Graph(id='g5', figure=fig5, style={'display': 'inline-block'}),
+                ])
+            ], className="row"),
+            #dcc.Graph(figure=fig1, style={'display': 'inline-block'}),
+            #dcc.Graph(figure=fig2, style={'display': 'inline-block'}),
+
+            dash_table.DataTable(
+                data.to_dict('records'),
+                [{'name':i, 'id':i} for i in data.columns],
+                export_format="csv"
+            ),
+            html.Hr(),
+        ])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
