d245445a3949fb85271234b2855610835f4883c1
wpeguero <wpeguerorosario@outlook.com>
2022-12-02 21:28:24 -0500

    NEW:Test the rescale function
    Started testing the function that rescales the image based on the pixel
    width to pixel height ratio. This test assures that the scaling of the
    image is exactly a factor of 2.

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,5 +1,6 @@
 """Module for testing the pipeline library."""
 from pipeline import *
+from numpy import ndarray
 import pytest
 
 file = "./data/CMMD-set/sample_data/D1-0820_1-4.dcm"
@@ -21,7 +22,13 @@ def test_transform_data():
 
 def test_rescale_image():
     """Test whether images are rescaled appropriately."""
-    pass
+    datapoint = extract_data(file)
+    img = rescale_image(datapoint.get('image'))
+    assert type(img) == ndarray
+    ishape = img.shape
+    oshape = datapoint['image'].shape
+    factor = oshape[0] / ishape[0]
+    assert factor == 2
 
 
 if __name__ == "__main__":
