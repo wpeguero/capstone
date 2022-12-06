be9aa8134605f0fe10f019db66412ac57f957af8
wpeguero <wpeguerorosario@outlook.com>
2022-12-06 17:19:00 -0500

    CHANGE: Developed function to test model accuracy.
    Developed a function for testing the accuracy of the models. Changed the
    model output fixture function to sample 1,000 subjects instead of using
    only 1 sample. Completely changed how the model output was tested after
    experimenting, this new algorithm allows one to make sure that the
    outputs sum up to 1.0 (this is because they are meant to be
    probabilities).

--- test_models.py
+++ test_models.py
@@ -1,7 +1,7 @@
 """Test models and model library for capstone project."""
 from models import *
-from pipeline import load_testing_data, predict
-from tensorflow.keras.models import Model
+from pipeline import load_testing_data, predict, calculate_confusion_matrix
+from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.losses import CategoricalCrossentropy
 import pytest
 
@@ -15,22 +15,33 @@ pytest.fixture
 def create_test_data():
     """Loads the data for testing purposes."""
     filename = "data/CMMD-set/test.csv"
-    tdata = load_testing_data(filename, sample_size=1)
+    tdata = load_testing_data(filename, sample_size=1_000)
     return tdata
 
-def test_model_output(create_model):
+@pytest.fixture
+def test_model_output(create_test_data):
+    """Test whether the output is recorded within an array."""
+    inputs, output = tumor_classifier(1147,957)
+    model = Model(inputs=inputs, outputs=output)
+    model.compile(optimizer='Adam', loss=CategoricalCrossentropy())
+    pdf = model.predict(create_test_data[0])
+    assert type(pdf['score'].values[0]) == list()
+
+@pytest.fixture
+def test_model_score_probability(create_test_data):
     """Test whether the output of the models are a set of probabilities."""
     inputs, output = tumor_classifier(1147,957)
     model = Model(inputs=inputs, outputs=output)
     model.compile(optimizer='Adam', loss=CategoricalCrossentropy())
-    prediction = model.predict(create_model[0])
+    pdf = model.predict(create_test_data[0])
+    assert sum(pdf['score'].values[0]) == 1.0
 
+@pytest.fixture
+def test_model_accuracy(create_test_data):
+    """Test whether the accuracy of the most recent model reaches the standard."""
+    pdf = predict(create_test_data, 'models/tclass_VGG8')
+    ct, metrics = calculate_confusion_matrix(pdf)
+    assert metrics['Accuracy'] >= 0.90
 
 if __name__ == "__main__":
-    #pytest.main()
-    inputs, output = tumor_classifier(1147,957)
-    model_name = "models/tclass_VGG7"
-    tdata = create_test_data()
-    pdf = predict(tdata, model_name)
-    print(pdf['score'].tolist())
-    print(type(pdf['score'].tolist()))
+    pytest.main()
