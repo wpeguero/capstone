bdb25c69fd4b2374309b4dae66c1619a38f61bd3
wpeguero <wpeguerorosario@outlook.com>
2022-12-08 19:39:40 -0500

    NEW: algorithms for visualizing training eval.

---
+++ visuals.py
@@ -0,0 +1,10 @@
+"""Set of functions and algorithms to observe model training."""
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def _main():
+    pass
+
+
+if __name__ == "__main__":
+    _main()
